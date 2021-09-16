17dd49356bcf89a6c500a0b0ae586df179d0b612
Mikkel Christiansen <mikkel.christiansen250996@gmail.com>
2021-09-16 15:22:37 +0200

    Updated image download endpoint

--- pkg/api/image.go
+++ pkg/api/image.go
@@ -7,13 +7,17 @@ import (
 )
 
 func imageSubRouter(r *mux.Router) {
-	imageRouter := r.PathPrefix("/images").Subrouter()
+	imageRouter := r.PathPrefix("/api/images").Subrouter()
 	//imageRouter.Handle("/", http.StripPrefix("/images/", http.FileServer(http.Dir("./images"))))
-	imageRouter.Queries("download","{download:[0-9]+}").HandlerFunc(downloadImage).Methods("GET", "OPTIONS").Name("download")
+	imageRouter.Queries("download","{download:[0-9]+}").HandlerFunc(tokenAuthMiddleware(downloadImage)).Methods("GET", "OPTIONS").Name("download")
 
 }
 
 func downloadImage(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+	if r.Method == "OPTIONS" {
+		return
+	}
 	image := mux.Vars(r)["download"]
 	log.Logger.Debug().Msgf("User wants to download image: %s", image)
 	w.Header().Set("Content-Disposition", "inline; filename=raspberrypi.img")
