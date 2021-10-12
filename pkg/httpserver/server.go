b5426fc415083b4c166e5bd24a07eaf63f33d1ad
Mikkel Christiansen <mikkel.christiansen250996@gmail.com>
2021-10-12 14:37:30 +0200

    Removed websocket, switched to polling

--- pkg/httpserver/server.go
+++ pkg/httpserver/server.go
@@ -4,7 +4,6 @@ import (
 	"github.com/Mikkelhost/Gophers-Honey/pkg/api"
 	"github.com/Mikkelhost/Gophers-Honey/pkg/config"
 	log "github.com/Mikkelhost/Gophers-Honey/pkg/logger"
-	"github.com/Mikkelhost/Gophers-Honey/pkg/websocket"
 	"github.com/gorilla/mux"
 	sLog "log"
 	"net/http"
@@ -17,7 +16,7 @@ var configured bool
 func RunServer(c *config.Config) {
 	log.Logger.Debug().Msgf("Starting websocket")
 	r := mux.NewRouter()
-	websocket.SetupRouter(r)
+	api.SetupRouter(r)
 	api.SetupRouters(r, c)
 	if !DEV {
 		sLog.Fatal(http.ListenAndServeTLS(":8443", "certs/nginx-selfsigned.crt", "certs/nginx-selfsigned.key", r))
